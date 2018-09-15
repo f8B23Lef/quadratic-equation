c2a057b51ad6f0fab2eb798a2c1b20d4907ac4f4
f8B23Lef <mal7rish@yandex.ru>
2018-09-16 00:11:28 +0300

    quadratic-equation/implement quadratic equation solver function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+  equation = equation.replace(/\s/g, "");
+
+  var regExp = /([+-]?\d+)\*x\^2([+-]?\d+)\*x([+-]?\d+)/;
+  var matchArr = equation.match(regExp);
+  
+  var a = matchArr[1];
+	var b = matchArr[2];
+  var c = matchArr[3];
+ 
+  /*
+	var parts = equation.split("*x^2");
+  var a = parts[0];
+	var b = parts[1].split("*x")[0];
+  var c = parts[1].split("*x")[1];
+  */
+
+	var x1, x2;
+
+	var d = Math.pow(b,2) - 4 * a * c;
+	
+	if(d > 0) {
+		x1 = (-b + Math.sqrt(d)) / (2 * a);
+		x2 = (-b - Math.sqrt(d)) / (2 * a);
+	} else if(d == 0) {
+		x1 = (-b) / (2 * a);
+		x2 = x1;
+  } 
+      
+  var resArr = [];
+	resArr.push(Math.round(x1), Math.round(x2));
+	resArr.sort(function(a, b) {
+    return a - b
+  });
+	
+	return resArr;
 }
