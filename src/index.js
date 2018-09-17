23cbee0e736a4ed787f38042708eaa7e90a53afe
f8B23Lef <mal7rish@yandex.ru>
2018-09-17 23:47:01 +0300

    quadratic-equation/fix: tab size

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ module.exports = function solveEquation(equation) {
   var matchArr = equation.match(regExp);
   
   var a = matchArr[1];
-	var b = matchArr[2];
+  var b = matchArr[2];
   var c = matchArr[3];
  
   /*
@@ -16,23 +16,23 @@ module.exports = function solveEquation(equation) {
   var c = parts[1].split("*x")[1];
   */
 
-	var x1, x2;
+  var x1, x2;
 
-	var d = Math.pow(b,2) - 4 * a * c;
+  var d = Math.pow(b,2) - 4 * a * c;
 	
-	if(d > 0) {
-		x1 = (-b + Math.sqrt(d)) / (2 * a);
-		x2 = (-b - Math.sqrt(d)) / (2 * a);
-	} else if(d == 0) {
-		x1 = (-b) / (2 * a);
-		x2 = x1;
+  if(d > 0) {
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+  } else if(d == 0) {
+    x1 = (-b) / (2 * a);
+    x2 = x1;
   } 
       
   var resArr = [];
-	resArr.push(Math.round(x1), Math.round(x2));
-	resArr.sort(function(a, b) {
+  resArr.push(Math.round(x1), Math.round(x2));
+  resArr.sort(function(a, b) {
     return a - b
   });
 	
-	return resArr;
+  return resArr;
 }
